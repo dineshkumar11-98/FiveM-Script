67374e0cb31ddee8e00f9cc42b28ab54dcd7d2e9
dinesh kumar <54545814+dineshkumar11-98@users.noreply.github.com>
2022-01-21 10:30:25 +0530

    Add files via upload

---
+++ esx_airvehicleradar/config.lua
@@ -0,0 +1,16 @@
+marker = {
+    markerType = 34,
+    markerPos = {x = -1058.856, y = -2929.458, z = 82.58755},
+    markerScale = 1.0,
+    markerColor = {r = 0, g = 255, b = 0, a = 50},
+}
+
+heli = {
+    blipSprite = 422,
+    blipColor = 2,
+}
+
+aeroplane = {
+    blipSprite = 423,
+    blipColor = 2,
+}
